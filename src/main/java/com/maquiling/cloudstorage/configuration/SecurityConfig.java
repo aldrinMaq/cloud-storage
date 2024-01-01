c662c0e82cf3c3a2d9a6d537a32bdbb73739dfed
Aldrin <aamaquiling@nemsu.edu.ph>
2024-01-01 19:35:10 +0800

    update cors

--- src/main/java/com/maquiling/cloudstorage/configuration/SecurityConfig.java
+++ src/main/java/com/maquiling/cloudstorage/configuration/SecurityConfig.java
@@ -24,10 +24,7 @@ public class SecurityConfig  {
         http
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of(
-                            "https://your-cloud-maquiling.vercel.app", // Add your frontend's origin
-                            "http://localhost:5173" // Keep this for local testing
-                    ));
+                    config.setAllowedOrigins(List.of("*")); // Allow all origins
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
